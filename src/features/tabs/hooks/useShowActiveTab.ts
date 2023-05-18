280fba19dbddc363e339401f5b2a7b33310e7aed
AshishPandagre <ashishpandagre9@gmail.com>
2023-05-18 10:13:43 +0530

    fixed scrolling to active tab

--- src/features/tabs/hooks/useShowActiveTab.ts
+++ src/features/tabs/hooks/useShowActiveTab.ts
@@ -6,14 +6,22 @@ export default function useShowActiveTab(ref: ParamType) {
   useEffect(() => {
     if (ref && ref.current) {
       const targetNode = ref.current;
-      const config = { attributes: true, childList: false, subtree: true };
-      const callback = (mutationList: any, observer: MutationObserver) => {
+      const config = { attributes: true, childList: true, subtree: true };
+
+      const callback = (
+        mutationList: any,
+        observer: MutationObserver
+      ) => {
         for (const mutation of mutationList) {
-          if (mutation.target.classList.contains("border-red-400")) {
-            mutation.target.scrollIntoView();
-          }
+          let target;
+          if (mutation.type == "childList" && mutation.addedNodes.length > 0)
+            target = mutation.addedNodes[0];
+          if (mutation.type=="attributes" && mutation.target.classList.contains("border-red-400"))
+            target = mutation.target;
+          if(target) target.scrollIntoView();
         }
       };
+
       const observer = new MutationObserver(callback);
       observer.observe(targetNode, config);
     }
